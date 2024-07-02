c8c15f6c529bb26b4a43842d2d2d34c74efddf3a
zhangzeyu <akihachang@gmail.com>
2024-07-02 11:32:20 +0800

    整合Thymeleaf与日志

--- SpringBootWebTest/src/main/java/com/zzy/config/SecurityConfiguration.java
+++ SpringBootWebTest/src/main/java/com/zzy/config/SecurityConfiguration.java
@@ -1,28 +1,43 @@
 package com.zzy.config;
 
 import com.zzy.service.impl.UserAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Resource
     private UserAuthService userAuthService;
+    @Resource
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/static/**").permitAll()
                 .anyRequest().hasAnyRole("user", "admin")
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/doLogin")
+                .permitAll()
+                .defaultSuccessUrl("/index")
+                .and()
+                .rememberMe()
+                .tokenRepository(new JdbcTokenRepositoryImpl(){{setDataSource(dataSource);}});
+
     }
 
     @Override
--- SpringBootWebTest/src/main/java/com/zzy/controller/MainController.java
+++ SpringBootWebTest/src/main/java/com/zzy/controller/MainController.java
@@ -1,16 +1,31 @@
 package com.zzy.controller;
 
 import com.zzy.entity.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @Controller
 public class MainController {
 
     @Value("${test.name}")
     String testValue;
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request) {
+        MDC.put("reqId", request.getSession().getId());
+        log.info("有人访问了login页面");
+        return "login";
+    }
 
     @RequestMapping("/index")
     public String index() {
